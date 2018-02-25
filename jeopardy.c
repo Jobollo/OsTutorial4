14240b6e7047f677f7d4140505251e707e219c67
DragoniteX1 <joshua.pineda1@uoit.net>
2018-02-25 12:13:17 -0500

    Jeopardy files
    Initial work done on the functions

---
+++ jeopardy.c
@@ -0,0 +1,71 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 4
+
+// Put global environment variables here
+
+// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+void tokenize(char *input, char **tokens);
+
+// Displays the game results for each player, their name and final score, ranked from first to last place
+void show_results(struct player *players, int num_players);
+
+int game_state;
+
+
+int main(int argc, char *argv[])
+{
+    // An array of 4 players, may need to be a pointer if you want it set dynamically
+    struct player players[NUM_PLAYERS];
+
+    // EXAMPLE: player 1 is named Fred
+    strcpy(players[0].name, "Fred");
+    printf("%s\n", players[0].name);
+    
+    // Buffer for user input
+    char buffer[BUFFER_LEN] = { 0 };
+
+    // Display the game introduction and initialize the questions
+    initialize_game();
+
+    // Prompt for players names
+    printf("Time to start the game! Type out the names of each player.");
+    // initialize each of the players in the array
+    for (int i = 0; i < NUM_PLAYERS; i ++)
+    {
+        printf("Type in Player %d's name: ", i + 1);
+        scanf("%s", players[i].name);
+        players[i].score = 0;
+    }
+
+    // Perform an infinite loop getting command input from users until game ends
+    game_state = 1;
+    while (game_state)
+    {
+        // EXAMPLE: This line gets a line of input from the user
+        fgets(buffer, BUFFER_LEN, stdin);
+        printf("[before]%s[after]", buffer);
+
+        // Call functions from the questions and players source files
+
+        // Execute the game until all questions are answered
+
+        // Display the final results and exit
+    }
+    return EXIT_SUCCESS;
+}
---
+++ players.c
@@ -0,0 +1,45 @@
+/*
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "players.h"
+
+struct player {
+    char name[MAX_LEN];
+    int score;
+};
+
+// Returns true if the player name matches one of the existing players
+bool player_exists(struct player *players, int num_players, char *name)
+{
+    for (int i = 0; i < num_players; i ++)
+    {
+        if (players[i].name == name)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Go through the list of players and update the score for the 
+// player given their name
+void update_score(struct player *players, int num_players, char *name, int score)
+{
+
+    for (int i = 0; i < num_players; i ++)
+    {
+        if (players[i].name == name)
+        {
+            players[i].score = score;
+        }
+    }
+    
+}
